f61b7dbf7530b7b04611e655804f1640360aa8f0
ymis1080 <iwayma1880@gmail.com>
2013-05-08 23:01:37 +0900

    add gemrc and gitconfig

--- init-server.sh
+++ init-server.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# post symbolic link of setting files
+for dotfile in ".vimrc" ".vim" ".zshrc" ".tmux.conf" ".gitconfig" ".gemrc"
+do  
+  ln -s $HOME/dotfile/${dotfile} $HOME/${dotfile}
+done
+
 # rubyをCentOSに入れるために必要な色々のインストール
 sudo yum -y install git make gcc gcc-c++\
   zlib-devel openssl-devel httpd-devel\
@@ -10,4 +16,5 @@ sudo yum -y install git make gcc gcc-c++\
 # rvm (bash)
 bash < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)
 
-## append .bash_profile [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+## may need to append this comand to .bash_profile
+# "[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
