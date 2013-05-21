77be6bbb38ffa9a6034109514de88deaf60f3718
Hiromasa IWAYAMA <hiwayama@chaosiwayama-no-MacBook-Pro.local>
2013-05-21 18:24:12 +0900

    bug fix

--- set-link.sh
+++ set-link.sh
@@ -3,6 +3,6 @@
 # post symbolic link of setting files
 for dotfile in ".vimrc" ".vim" ".zshrc" ".tmux.conf" ".gitconfig" ".gemrc"
 do  
-  ln -s $HOME/dotfile/${dotfile} $HOME/${dotfile}
+  ln -s $HOME/dotfiles/${dotfile} $HOME/${dotfile}
 done
 
