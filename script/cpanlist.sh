837a3eb03318bd05f9101553c79b5ffd432e98e3
ymis1080 <iwayma1880@gmail.com>
2014-04-08 23:53:37 +0900

    Added install script for cpan libraries

---
+++ script/cpanlist.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# for perlbrew
+# $ curl -L http://install.perlbrew.pl | bash
+# $ perlbrew init
+
+# for cpanm
+# $ perlbrew install-cpanm
+
+# WebFW
+cpanm Amon2
+cpanm Teng
+
+# Development
+cpanm Carton
+cpanm Perl::Tidy
+cpanm Env::Config
+cpanm Data::Dumper
+cpanm Term::ANSIColor
