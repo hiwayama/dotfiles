3444ffbd83da1250f0287dd8ac05c73f12af8fa0
ymis1080 <iwayma1880@gmail.com>
2014-04-13 05:56:58 +0900

    added tree

--- script/init-server.sh
+++ script/init-server.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-# rubyをCentOSに入れるために必要な色々のインストール
 sudo yum -y install git make gcc gcc-c++\
   zlib-devel openssl-devel httpd-devel\
   curl curl-devel readline-devel tk-devel\
   libxml2-devel libxslt-devel nkf\
-  libyaml-devel zsh
+  libyaml-devel zsh tree
 
 # rvm (bash)
-bash < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)
+#bash < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)
 
 ## may need to append this comand to .bash_profile
 # "[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
