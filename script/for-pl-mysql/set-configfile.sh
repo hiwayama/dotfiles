3607d8ada71678ed9c03748d8576054c1cd268d8
ymis1080 <iwayma1880@gmail.com>
2014-04-16 14:22:36 +0900

    fixed typo

--- script/for-pl-mysql/set-configfile.sh
+++ script/for-pl-mysql/set-configfile.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
+# 
+# adduser hiwayama
+# passwd hiwayama
+# visudo
+# su - hiwayama
+
 # ---------
 # 設定ファイルの配置
 # tmuxを起動してyum -y update中に実行したい...
 # rootから!!!
+yum -y install mysql mysql-devel mysql-server\
+ httpd httpd-server httpd-devel perl-DBI perl-DBD-MySQL
 
 # 元ファイルのバックアップ
 cp /etc/sysconfig/iptables /etc/sysconfig/iptables.origin
@@ -11,16 +19,12 @@ cp /etc/ssh/sshd_config /etc/ssh/sshd_config.origin
 cp /etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd.conv.origin
 cp /etc/httpd/conf.d/fcgid.conf /etc/httpd/conf.d/fcgid.conf.origin
 cp /etc/my.conf /etc/my.conf.origin
-service restart iptables
-service restart sshd
-service restart httpd
-service restart mysqld
 
 # 設定ファイルの配置
 cp -f ./conf/iptables /etc/sysconfig/
 cp -f ./conf/sshd_config /etc/ssh/
 cp -f ./conf/my.conf /etc/
-cp -f ./conf/httpd.conf /etc/httpd/
+cp -f ./conf/httpd.conf /etc/httpd/conf/
 cp -f ./conf/fcgid.conf /etc/httpd/conf.d/
 
 # apache, FCGIの動作確認でつかうファイルの配置
@@ -28,7 +32,6 @@ cp -f ./check-pl-script/test.html /var/www/html/
 cp -f ./check-pl-script/test.pl /var/www/cgi-bin/
 cp -f ./check-pl-script/test.fpl /var/www/cgi-bin/
 
-cd ~/
 service restart iptables
 service restart sshd
 service restart httpd
