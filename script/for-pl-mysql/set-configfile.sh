d71e2ea66cfc22baf020111b202c9c91563866d7
ymis1080 <iwayma1880@gmail.com>
2014-04-18 15:19:40 +0900

    fixed path

--- script/for-pl-mysql/set-configfile.sh
+++ script/for-pl-mysql/set-configfile.sh
@@ -23,16 +23,16 @@ cp /etc/httpd/conf.d/fcgid.conf /etc/httpd/conf.d/fcgid.conf.origin
 cp /etc/my.conf /etc/my.conf.origin
 
 # 設定ファイルの配置
-cp -f ./conf/iptables /etc/sysconfig/
-cp -f ./conf/sshd_config /etc/ssh/
-cp -f ./conf/my.conf /etc/
-cp -f ./conf/httpd.conf /etc/httpd/conf/
-cp -f ./conf/fcgid.conf /etc/httpd/conf.d/
+cp -f $HOME/dotfiles/script/for-pl-mysql/conf/iptables /etc/sysconfig/
+cp -f $HOME/dotfiles/script/for-pl-mysql/conf/sshd_config /etc/ssh/
+cp -f $HOME/dotfiles/script/for-pl-mysql/conf/my.conf /etc/
+cp -f $HOME/dotfiles/script/for-pl-mysql/conf/httpd.conf /etc/httpd/conf/
+cp -f $HOME/dotfiles/script/for-pl-mysql/conf/fcgid.conf /etc/httpd/conf.d/
 
 # apache, FCGIの動作確認でつかうファイルの配置
-cp -f ./check-pl-script/test.html /var/www/html/
-cp -f ./check-pl-script/test.pl /var/www/cgi-bin/
-cp -f ./check-pl-script/test.fpl /var/www/cgi-bin/
+cp -f $HOME/dotfiles/script/for-pl-mysql/check-pl-script/test.html /var/www/html/
+cp -f $HOME/dotfiles/script/for-pl-mysql/check-pl-script/test.pl /var/www/cgi-bin/
+cp -f $HOME/dotfiles/script/for-pl-mysql/check-pl-script/test.fpl /var/www/cgi-bin/
 
 service iptables restart
 service sshd restart
