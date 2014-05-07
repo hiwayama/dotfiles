426478122f4781fec4026705a4671ae47e2a2738
ymis1080 <iwayma1880@gmail.com>
2014-05-08 08:51:44 +0900

    anyenv設定ファイル追加

---
+++ script/anyenv-init.sh
@@ -0,0 +1,15 @@
+
+# rbenv
+anyenv install plenv 
+
+# plenv
+anyenv install plenv 
+
+# ndenv (node)
+anyenv install ndenv
+exec $SHELL -l
+ndenv install v0.10.28
+ndenv global v0.10.28
+ndenv local v0.10.28
+npm install mysql bower grunt grunt-contrib grunt-cli -g
+
