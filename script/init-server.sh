506ade30f41d63ef01f523583ff4f5a3bdb45376
H.IWAYAMA <1152796+hiwayama@users.noreply.github.com>
2021-03-15 15:48:37 +0900

    不要ファイル削除

--- script/init-server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-rpm -Uvh http://ftp-srv2.kddilabs.jp/Linux/distributions/fedora/epel/6/i386/epel-release-6-8.noarch.rpm
-sudo yum -y install git make gcc gcc-c++\
-  zlib-devel openssl-devel httpd-devel\
-  curl curl-devel readline-devel tk-devel\
-  libxml2-devel libxslt-devel nkf dstat\
-  libyaml-devel zsh tree vim tmux tig --enablerepo=epel
-
